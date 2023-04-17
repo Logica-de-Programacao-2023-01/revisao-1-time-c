1db4724fbf5543e09468841eae68bd506abc2d5f
Gabriel Antonio Navarro Paiva <gabriel.npaiva@dinfor.uniceub>
2023-04-17 10:58:03 -0300

    respostas

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,38 @@
 package q2
 
+import (
+	"fmt"
+	"strings"
+)
+
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+
+	soma := 0.0
+	media := 0.0
+
+	apenasLetras := strings.ReplaceAll(text, "!", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, ",", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, ".", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, "(", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, ")", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, "[", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, "]", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, "{", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, "}", "")
+	apenasLetras = strings.ReplaceAll(apenasLetras, "?", "")
+
+	sliceText := strings.Split(apenasLetras, " ")
+
+	if len(sliceText) == 0 {
+		return media, fmt.Errorf("texto vazio")
+	}
+
+	for _, ranSliceText := range sliceText {
+		soma += float64(len(ranSliceText))
+	}
+
+	media = soma / float64(len(sliceText))
+
+	return media, nil
+
 }
--- q3/q3.go
+++ q3/q3.go
@@ -1,6 +1,34 @@
 package q3
 
+import "fmt"
+
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	return 0, 0, 0, nil
+
+	if len(numbers) == 0 {
+		return 0, 0, 0, fmt.Errorf("lista vazia")
+	}
+
+	i := 0
+	maior := numbers[0]
+	menor := numbers[0]
+	soma := 0.0
+	media := 0.0
+
+	for i = 0; i < len(numbers); i++ {
+
+		if numbers[i] > maior {
+			maior = numbers[i]
+		}
+
+		if numbers[i] < menor {
+			menor = numbers[i]
+		}
+
+		soma += float64(numbers[i])
+	}
+
+	media = (soma - float64(maior) - float64(menor)) / float64(len(numbers)-2)
+
+	return menor, maior, media, nil
+
 }
